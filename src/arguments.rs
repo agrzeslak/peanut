460e5322c90a7c45103ea67d601774d03e465ec4
Andrzej Grzeslak <agrzeslak0@gmail.com>
2022-11-18 15:43:42 +1100

    Main and arguments: forgot to previously add.
    Only a rough skeleton just to test functionality.

---
+++ src/arguments.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ValueHint};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Arguments {
+    /// Assembly file to be executed.
+    #[arg(value_hint = ValueHint::FilePath)]
+    pub file_path: PathBuf, 
+}
